daccdac78b7f4b7f6117c699a81b8ad4c49760ea
benjamin-allen <beallen529@gmail.com>
2020-02-18 15:05:48 -0600

    fix routing?

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("dummy", (request, response) => {
+router.get("/dummy", (request, response) => {
   response.body = "DUMMY";
 });
 
