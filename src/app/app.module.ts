0eb42d48166509ce66740a90a7c67775070a5121
Brad Greenway <sbgreenw@uark.edu>
2020-02-24 19:43:23 -0600

    learning how much i don't know about angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { SignInComponent } from './components/signin/signin.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
 import { ProductListingComponent } from './components/product-listing/product-listing.component';
@@ -14,6 +15,7 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
   declarations: [
     AppComponent,
     WelcomeComponent,
+	SignInComponent,
     MainMenuComponent,
     EmployeeDetailComponent,
     ProductListingComponent,
@@ -21,6 +23,7 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
   ],
   imports: [
     BrowserModule,
+	ReactiveFormsModule,
     HttpClientModule,
     BooksModule
   ],
