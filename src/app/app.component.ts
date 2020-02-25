5d43763c76112d9824f44c79afa5841a1c1e0755
Brad Greenway <sbgreenw@uark.edu>
2020-02-24 19:37:57 -0600

    bugfixing component stuff pt.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  imports: [ReactiveFormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
