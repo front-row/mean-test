fe98ea019f0b14aac75376ca18ed251d29a35fca
Brad Greenway <sbgreenw@uark.edu>
2020-02-24 19:19:56 -0600

    Added a sign in component and a basic form to populate it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+@NgModule({
+  imports: [ReactiveFormsModule]
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
