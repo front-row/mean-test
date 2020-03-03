bd85e44b29de820c07dd3d93362eceb308aea3fe
Brad Greenway <sbgreenw@uark.edu>
2020-03-02 18:54:20 -0600

    Finished signin view; lacking Styles for now.

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'sign-in',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
-})
+  })
+
 export class SignInComponent {
   signInForm = new FormGroup({
-    username: new FormControl(''),
-    psswd: new FormControl(''),
+    id: new FormControl(''),
+    password: new FormControl(''),
   });
 
+constructor(private http: HttpClient) 
+{
+}
 
-  onSubmit() 
+  onSubmit(loginData) 
   {
-    // TODO: Actually make this do a POST request I guess
-    console.log("Congrats this does nothing yet");
+    console.log('Signin request has been sent.');
+    let login = {};
+    login['id'] = loginData.id;
+    login['password'] = loginData.password;
+	console.log(login['id'] + " " + login['password']);
+    return this.http.post('**UNFINISHED**', login).subscribe(response => console.log(response));
   }
 }
 
