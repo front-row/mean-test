30b1de4eef82733998789b76f0db224c30b096e7
kyle sadler <kylesadler5@gmail.com>
2020-03-02 19:24:37 -0600

    redirecting if no account

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
@@ -6,11 +7,23 @@ import { FormGroup, FormControl } from '@angular/forms';
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit{
   signInForm = new FormGroup({
     username: new FormControl(''),
     psswd: new FormControl(''),
   });
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    // get if user is signed in
+    var signedIn = true;
+    
+    if(!signedIn){
+      this.router.navigate(['employeedetail']);
+    }
+  }
+
 
 
   onSubmit() 
