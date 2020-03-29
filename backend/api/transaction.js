f1aae8fff823d6e6327d555cdeed7eebf68d5c3c
Benjamin Allen <beallen529@gmail.com>
2020-03-29 01:15:43 -0500

    Add placeholder functions for backend team

--- backend/api/transaction.js
+++ backend/api/transaction.js
@@ -1,4 +1,31 @@
 const util = require('../util.js');
 
 module.exports = {
+    getAllTransactions: function(request, response) {
+
+    },
+
+    getTransaction: function(request, response) {
+
+    },
+
+    createTransaction: function(request, response) {
+
+    },
+
+    deleteTransaction: function(request, response) {
+
+    },
+
+    addProduct: function(request, response) {
+
+    },
+
+    removeProduct: function(request, response) {
+
+    },
+
+    editQuantity: function(request, response) {
+        
+    }
 };
